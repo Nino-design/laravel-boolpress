0781ad80251a70c7417b5d12e7391af593ba53ca
Nino-design <npompili@gmail.com>
2022-07-13 15:54:34 +0100

    pagina home creata

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Pagina home</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+   
+};
+
+
+</script>
+
+<style>
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,15 @@
 <template>
-    <div>
-        <Posts/>
-    </div>
+<div>
+    <router-view></router-view>
+</div>
+    
 </template>
 
 <script>
-import Posts from '../components/Posts.vue';
+
 export default {
     name:'App',
-    components: {
-        Posts,
-    }
+ 
 };
 
 
